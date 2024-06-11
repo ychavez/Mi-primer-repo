ee61b6d13fd56638a1acfaedc65dbdefd11c1674
Sandy-Rivera <62038696+Sandy-Rivera@users.noreply.github.com>
2024-06-10 18:34:14 -0600

    multiplicación

--- Program.cs
+++ Program.cs
@@ -13,3 +13,11 @@ if(operacion == "Dividr")
     System.Console.WriteLine("El resultado de la division es: " + (num1 / num2));
 }
 
+if(operacion == "Multiplicar")
+{
+    System.Console.WriteLine("Escribe el primer numero: ");
+    int num1 = int.Parse(Console.ReadLine());
+    System.Console.WriteLine("Escribe el segundo numero: ");
+    int num2 = int.Parse(Console.ReadLine());
+    System.Console.WriteLine("El resultado de la division es: " * (num1 / num2));
+}
